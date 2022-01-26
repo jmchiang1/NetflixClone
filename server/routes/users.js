915e5ff1156c91e703f5b7907f37f4776a629d51
Jonathan Chiang <jonathanchiang7@gmail.com>
2022-01-26 17:32:41 -0500

    Added Update user route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,27 +1,26 @@
 const router = require("express").Router();
-const User = require("../models/User");
+const User = require("../models/UserModel");
 const CryptoJS = require("crypto-js");
 const verifyToken = require("../verifyToken");
 
 //UPDATE USER
-router.put("/:id", verifyToken, async (req, res) => {
-  if (req.user.id === req.params.id || req.user.isAdmin) {
-    if (req.body.password) {
+router.put("/:id", verifyToken, async (req, res) => {       //verify token first 
+  if (req.user.id === req.params.id || req.user.isAdmin) {  //if user id's match or user is an Admin
+    if (req.body.password) {                                //if password being changed, encrypt it 
       req.body.password = CryptoJS.AES.encrypt(
         req.body.password,
         process.env.SECRET_KEY
       ).toString();
     }
-
     try {
-      const updatedUser = await User.findByIdAndUpdate(
+      const updatedUser = await User.findByIdAndUpdate( //mongoose function: findByIdAndUpdate
         req.params.id,
         {
-          $set: req.body,
+          $set: req.body,   //$set operator replaces the value of a field with the specified value
         },
-        { new: true }
+        { new: true }       //return new user after updating 
       );
-      res.status(200).json(updatedUser);
+      res.status(200).json(updatedUser);  //send back updatedUser object 
     } catch (err) {
       res.status(500).json(err);
     }
