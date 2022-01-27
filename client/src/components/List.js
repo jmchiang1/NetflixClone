4fdb97e67a66916585c84754d399227f26af784d
Jonathan Chiang <jonathanchiang7@gmail.com>
2022-01-27 18:13:18 -0500

    sliding feature not working

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -4,16 +4,18 @@ import './Styles/List.scss'
 import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
 import ArrowBackIosOutlinedIcon from '@mui/icons-material/ArrowBackIosOutlined';
 
-export default function List({ list }) {
+export default function List() {
   const [isMoved, setIsMoved] = useState(false);
   const [slideNumber, setSlideNumber] = useState(0);
   const [clickLimit, setClickLimit] = useState(window.innerWidth / 230);
 
   const listRef = useRef();
+  // let DEFAULT_DISTANCE = 0;
 
   const handleClick = (direction) => {
     setIsMoved(true);
-    let distance = listRef.current?.getBoundingClientRect().x ?? 0 - 50;
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    // let distance = listRef.current?.getBoundingClientRect().x ?? 0 - 50 ;
     if (direction === "left" && slideNumber > 0) {
       setSlideNumber(slideNumber - 1);
       listRef.current.style.transform = `translateX(${230 + distance}px)`;
@@ -23,6 +25,7 @@ export default function List({ list }) {
       listRef.current.style.transform = `translateX(${-230 + distance}px)`;
     }
   };
+
   return (
     <div className="list">
       <span className="listTitle">Continue to watch</span>
@@ -33,14 +36,14 @@ export default function List({ list }) {
           style={{ display: !isMoved && "none" }}
         />
         <div className="container">
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
-            <ListItem />
+            <ListItem index={0} />
+            <ListItem index={1}/>
+            <ListItem index={2}/>
+            <ListItem index={3}/>
+            <ListItem index={4}/>
+            <ListItem index={5}/>
+            <ListItem index={6}/>
+            <ListItem index={7}/>
         </div>
         <ArrowForwardIosOutlinedIcon
           className="sliderArrow right"
--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,33 +1,49 @@
-import React from 'react'
-import './Styles/ListItem.scss'
-import PlayCircleFilledWhiteOutlinedIcon from '@mui/icons-material/PlayCircleFilledWhiteOutlined';
-import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import ThumbDownIcon from '@mui/icons-material/ThumbDown';
+import React, { useState } from "react";
+import "./Styles/ListItem.scss";
+import PlayCircleFilledWhiteOutlinedIcon from "@mui/icons-material/PlayCircleFilledWhiteOutlined";
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ThumbDownIcon from "@mui/icons-material/ThumbDown";
 
-function ListItem() {
-    return (
-        <div className='listItem'>
-            <img src="https://wallpaperboat.com/wp-content/uploads/2021/12/19/79926/spider-man-no-way-home-12.jpg" alt="" />
-            <div className="itemInfo">
-                <div className="icons">
-                    <PlayCircleFilledWhiteOutlinedIcon/>
-                    <AddOutlinedIcon/>
-                    <ThumbUpIcon/>
-                    <ThumbDownIcon/>
-                </div>
-                <itemInfoTop>
-                    <span>1 Hour 14 minutes</span>
-                    <span className='limit'>18+</span>
-                    <span>2021</span>
-                </itemInfoTop>
-                <div className="desc">
-                With Spider-Man's identity now revealed, our friendly neighborhood web-slinger is unmasked and no longer able to separate his normal life as Peter Parker from the high stakes of being a superhero. When Peter asks for help from Doctor Strange, the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.
-                </div>
-                <div className="genre">Action</div>
-            </div>
+function ListItem({index, item}) {
+  const [isHovered, setIsHovered] = useState(false);
+  const [movie, setMovie] = useState({});
+
+  return (
+    <div
+      className="listItem"
+      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <img
+        src="https://wallpaperboat.com/wp-content/uploads/2021/12/19/79926/spider-man-no-way-home-12.jpg"
+        alt=""
+      />
+      <div className="itemInfo">
+        <div className="icons">
+          <PlayCircleFilledWhiteOutlinedIcon />
+          <AddOutlinedIcon />
+          <ThumbUpIcon />
+          <ThumbDownIcon />
         </div>
-    )
+        <div className="itemInfoTop">
+          <span>1 Hour 14 minutes</span>
+          <span className="limit">18+</span>
+          <span>2021</span>
+        </div>
+        <div className="desc">
+          With Spider-Man's identity now revealed, our friendly neighborhood
+          web-slinger is unmasked and no longer able to separate his normal life
+          as Peter Parker from the high stakes of being a superhero. When Peter
+          asks for help from Doctor Strange, the stakes become even more
+          dangerous, forcing him to discover what it truly means to be
+          Spider-Man.
+        </div>
+        <div className="genre">Action</div>
+      </div>
+    </div>
+  );
 }
 
-export default ListItem
+export default ListItem;
