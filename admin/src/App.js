96940308da2604790a9470563f0defc1bb7f0dab
Jonathan Chiang <jonathanchiang7@gmail.com>
2022-01-31 18:55:18 -0500

    deleted old admin folder

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,18 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Topbar from "./components/Topbar";
 import Sidebar from "./components/Sidebar";
 import Home from "./components/Home";
+import UserList from "./components/UserList";
 
 function App() {
   return (
-    <div>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Home/>
-        {/* <div className="others">Other Pages</div> */}
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Topbar />
+        <div className="container">
+          <Sidebar />
+          <Route path="/" element={<Home />} />
+          <Route path="/user" element={<UserList/>} />
+        </div>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
