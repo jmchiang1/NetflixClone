a45dd9792d474628e0c504286d668ad37bad4386
Jonathan Chiang <jonathanchiang7@gmail.com>
2022-02-11 17:08:01 -0500

    changes

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const dotenv = require("dotenv");
 var bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
+const URL = "mongodb+srv://jmchiang:darklord5@mthree-mongo.ojzwn.mongodb.net/netflix?retryWrites=true&w=majority"
 
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
@@ -21,7 +22,7 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect(URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -34,17 +35,18 @@ app.use("/users", userRoute);
 app.use("/movies", movieRoute);
 app.use("/lists", listRoute);
 
+//Server production assets
 //middleware to direct express to "client" and "admin" folder
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/.client/build")));
-  app.use(express.static(path.join(__dirname, "/.admin/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "admin/build")));
 
   //redirect to client and admin paths
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/.client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/.admin/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "admin", "build", "index.html"));
   });
 }
 
